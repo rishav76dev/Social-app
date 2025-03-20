e41da77d4490b1cbeb12d794b17beb7ea5aeb9d4
rishav76dev <rjrishav037@gmail.com>
2025-03-20 22:55:24 +0530

    ui update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 import { SignedIn,SignedOut,SignInButton,UserButton,SignUpButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/Button";
+
 export default function Home() {
   return (
     <div>
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+      Homepage content
     </div>
   );
 }
